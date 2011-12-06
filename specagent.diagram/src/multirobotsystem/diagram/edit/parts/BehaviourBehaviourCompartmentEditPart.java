a93a0874e35864299ee8719422ac42852644c52d
Piotr Trojanek <piotr.trojanek@gmail.com>
2011-12-06 14:03:34 +0100

    BehaviourCompartment inherited from ShapeCompartment

---
+++ specagent.diagram/src/multirobotsystem/diagram/edit/parts/BehaviourBehaviourCompartmentEditPart.java
@@ -0,0 +1,87 @@
+package multirobotsystem.diagram.edit.parts;
+
+import multirobotsystem.diagram.edit.policies.BehaviourBehaviourCompartmentCanonicalEditPolicy;
+import multirobotsystem.diagram.edit.policies.BehaviourBehaviourCompartmentItemSemanticEditPolicy;
+import multirobotsystem.diagram.part.Messages;
+
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.emf.common.notify.Notification;
+import org.eclipse.gef.EditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editparts.ShapeCompartmentEditPart;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.CreationEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.DragDropEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.EditPolicyRoles;
+import org.eclipse.gmf.runtime.diagram.ui.editpolicies.ResizableCompartmentEditPolicy;
+import org.eclipse.gmf.runtime.diagram.ui.figures.ResizableCompartmentFigure;
+import org.eclipse.gmf.runtime.draw2d.ui.figures.ConstrainedToolbarLayout;
+import org.eclipse.gmf.runtime.notation.View;
+
+/**
+ * @generated NOT
+ */
+public class BehaviourBehaviourCompartmentEditPart extends
+	ShapeCompartmentEditPart {
+	
+	/**
+	 * @generated
+	 */
+	public static final int VISUAL_ID = 7001;
+
+	/**
+	 * @generated
+	 */
+	public BehaviourBehaviourCompartmentEditPart(View view) {
+		super(view);
+	}
+
+	/**
+	 * @generated
+	 */
+	protected boolean hasModelChildrenChanged(Notification evt) {
+		return false;
+	}
+
+	/**
+	 * @generated
+	 */
+	public String getCompartmentName() {
+		return Messages.BehaviourBehaviourCompartmentEditPart_title;
+	}
+
+	/**
+	 * @generated
+	 */
+	public IFigure createFigure() {
+		ResizableCompartmentFigure result = (ResizableCompartmentFigure) super
+				.createFigure();
+		result.setTitleVisibility(false);
+		return result;
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void createDefaultEditPolicies() {
+		super.createDefaultEditPolicies();
+		installEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE,
+				new ResizableCompartmentEditPolicy());
+		installEditPolicy(EditPolicyRoles.SEMANTIC_ROLE,
+				new BehaviourBehaviourCompartmentItemSemanticEditPolicy());
+		installEditPolicy(EditPolicyRoles.CREATION_ROLE,
+				new CreationEditPolicy());
+		installEditPolicy(EditPolicyRoles.DRAG_DROP_ROLE,
+				new DragDropEditPolicy());
+		installEditPolicy(EditPolicyRoles.CANONICAL_ROLE,
+				new BehaviourBehaviourCompartmentCanonicalEditPolicy());
+	}
+
+	/**
+	 * @generated
+	 */
+	protected void setRatio(Double ratio) {
+		if (getFigure().getParent().getLayoutManager() instanceof ConstrainedToolbarLayout) {
+			super.setRatio(ratio);
+		}
+	}
+
+}
